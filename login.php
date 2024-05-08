84f3611d1ddd8b2933b0472f7eaee51dcb30fc16
swapnilchaudhari007 <sc29823@gmail.com>
2024-05-08 08:35:34 +0530

    Initial commit

---
+++ login.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php 
+session_start();
+include('./db_connect.php');
+  ob_start();
+  // if(!isset($_SESSION['system'])){
+
+    $system = $conn->query("SELECT * FROM system_settings")->fetch_array();
+    foreach($system as $k => $v){
+      $_SESSION['system'][$k] = $v;
+    }
+  // }
+  ob_end_flush();
+?>
+<?php 
+if(isset($_SESSION['login_id']))
+header("location:index.php?page=home");
+
+?>
+<?php include 'header.php' ?>
+<body class="hold-transition login-page bg-Blue">
+<html>
+
+<body>
+
+<style>
+    #navbar {
+        position: relative;
+    }
+    #navbar img {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+</style>
+
+    <div class="body">
+	<div class="login-logo">
+        <img src="Bajajlogo.jpg" alt="Logo" class="logo" style="width: 100%; height: 10%; max-width: 100px;>  
+		 </div>
+
+<div class="login-box">
+  <div class="login-logo">
+  
+    <a href="#" class="text-black"><b><?php echo $_SESSION['system']['name'] ?> - BajajGroup</b></a>
+  </div>
+ 
+  
+ <!-- <div class="imgcontainer">
+ <!-- <img src="Bajajlogo.jpg" alt="Avatar" class="avatar">   -->
+  <!-- </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <form action="" id="login-form">
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" name="email" required placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" name="password" required placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="remember">
+              <label for="remember">
+                Remember Me
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+<script>
+  $(document).ready(function(){
+    $('#login-form').submit(function(e){
+    e.preventDefault()
+    start_load()
+    if($(this).find('.alert-danger').length > 0 )
+      $(this).find('.alert-danger').remove();
+    $.ajax({
+      url:'ajax.php?action=login',
+      method:'POST',
+      data:$(this).serialize(),
+      error:err=>{
+        console.log(err)
+        end_load();
+
+      },
+      success:function(resp){
+        if(resp == 1){
+          location.href ='index.php?page=home';
+        }else{
+          $('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
+          end_load();
+        }
+      }
+    })
+  })
+  })
+</script>
+<?php include 'footer.php' ?>
+
+</body>
+</html>
